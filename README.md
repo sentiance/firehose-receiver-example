cf8c5b4c5fc22bfea4ad8bfcd25bdc47eba0cbe5
Shaun <shuvophoenix@gmail.com>
2019-06-12 12:04:16 +0200

    NodeJS implementation. (#4)
    * NodeJS implementation.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Firehose Receiver Example Implementations
 
 This repository contains various implementations of a simple receiving server for Firehose webhooks. All servers are protected by basic-auth, handle gzipped json content, and, on successful processing, return 200 OK.
+
+Each implementation comes with its own readme to show how it can be set up and run.
---
+++ nodejs/index.js
@@ -0,0 +1,68 @@
+//Copyright 2019 Sentiance NV
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+'use strict'
+
+const http = require('http')
+
+const basicAuth = require('express-basic-auth')
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+const server = http.createServer(app)
+
+app.set('port', 3001)
+app.use(bodyParser.json())
+
+app.use('/webhook', basicAuth({
+    // Update these to change the username/password to authenticate against.
+    users: { 'sentiance': 'securepassword' }  })
+)
+
+app.post(
+    '/webhook',
+    (req, res) => {
+
+        console.log('Received webhook message')
+        _doSomething(req.body)
+        res.status(200).end()
+    }
+)
+
+// If you are to receive events from multiple Application IDs on a single endpoint, a route like this helps in
+// identifying the Application ID from which a message originates.
+app.post(
+    '/webhook/:app_id',
+    (req, res) => {
+
+        console.log(`Received webhook message from ${req.params.app_id}`)
+        console.log(req.body)
+        res.status(200).end()
+    }
+)
+
+function _doSomething(events) {
+
+    // Iterating over the envelope to get at the events.
+    for (const event of events.data) {
+
+        console.log(`Received event of type ${event.meta.message_type} at ${event.meta.message_timestamp}`)
+        console.log(event.data)
+    }
+}
+
+server.listen(app.get('port'), function() {
+    console.log(`Server listening on ${app.get('port')}`)
+})
